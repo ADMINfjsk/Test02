36cae7aafa65763c05eadb7b974dd21a3126e2d2
ADMINfjsk <867075071@qq.com>
2019-10-17 15:39:02 +0800

    v2.0

--- run_suite.py
+++ run_suite.py
@@ -3,6 +3,7 @@ import unittest
 
 from case.IHRM_emp import TestEmp
 from case.IHRM_user import TestUser
+from tools.HTMLTestRunner import HTMLTestRunner
 
 suite = unittest.TestSuite()
 suite.addTest(TestUser("test_2_login"))
@@ -13,5 +14,6 @@ suite.addTest(TestEmp("test_4_delete"))
 
 
 
-runner = unittest.TextTestRunner()
+with open("./report/report.html","wb") as f:
+    runner = HTMLTestRunner(f,title="我的测试报告",description="版本 v1.0")
 runner.run(suite)
